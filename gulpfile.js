1adc311191d959b68edcee5c6ae4ff0bfaa98dc2
Y-dash <y-dash@y-dash.net>
2021-02-25 05:47:05 +0900

    init

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+let gulp = require('gulp');
+let plumber = require('gulp-plumber');
+let sass = require('gulp-sass');
+let autoprefixer = require('gulp-autoprefixer');
+let cleanCss = require('gulp-clean-css');
+let ejs = require('gulp-ejs');
+let prettify = require('gulp-prettify');
+
+const EJS_TASL = 'ejs';
+const EJS_SRC_WATCHING_PATH = 'templates/**/*.ejs';
+const EJS_SRC_TARGET_PATH = [
+	'templates/**/*.ejs',
+	'!templates/**/_*.ejs'
+];
+const EJS_DIST_PATH = 'dist';
+
+const SCSS_TASK = 'scss';
+const SCSS_SRC_PATH = 'src/styles/*.scss';
+const SCSS_DIST_PATH = 'dist/styles';
+
+const COPY_TASK = 'copy';
+const COPY_SRC_PATH = ['src/static'];
+const COPY_DIST_PATH = 'dist';
+
+gulp.task(EJS_TASL, (done) => {
+	gulp.src(EJS_SRC_TARGET_PATH)
+		.pipe(plumber())
+		.pipe(ejs({}, {}, {
+			ext: '.html'
+		}))
+		.pipe(prettify())
+		.pipe(gulp.dest(EJS_DIST_PATH));
+	done();
+})
+
+gulp.task(SCSS_TASK, (done) => {
+	gulp.src(SCSS_SRC_PATH)
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(autoprefixer({
+			grid: true
+		}))
+		.pipe(cleanCss())
+		.pipe(gulp.dest(SCSS_DIST_PATH));
+	done();
+})
+
+gulp.task(COPY_TASK, (done) => {
+	gulp.src(COPY_SRC_PATH, {
+		base: 'src'
+	})
+		.pipe(plumber())
+		.pipe(gulp.dest(COPY_DIST_PATH));
+	done();
+});
+
+gulp.task('default', (done) => {
+	gulp.watch(EJS_SRC_WATCHING_PATH, gulp.parallel(EJS_TASL));
+	gulp.watch(SCSS_SRC_PATH, gulp.parallel(SCSS_TASK));
+	gulp.watch(COPY_SRC_PATH, gulp.parallel(COPY_TASK));
+	done();
+});
